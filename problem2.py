076da205254aaf01c54f4750c99ac0c7f5fcd475
kenny0805 <gdgzwkm@gmail.com>
2014-11-28 21:35:05 -0800

    problem 2

--- problem2.py
+++ problem2.py
@@ -1,1 +1,18 @@
+#coding: utf-8
 __author__ = 'zz'
+
+import re
+fin=open('train_msgs.txt','r')
+fout=open('output.csv','w')
+stopwords={}
+contractions={}
+dictionary={}
+res=[]
+
+for word in ["a","n","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"]:
+	stopwords[word]=None
+
+for line in fin:
+	line = re.sub("[!@#$%^&*(),./<>?;':\"'[]{}]","",line)
+	words = line.split()
+	print(words)
