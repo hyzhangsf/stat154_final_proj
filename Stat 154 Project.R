2407f8673dc61a5679bf1e6828ed2e98ceaf8632
kenny0805 <gdgzwkm@gmail.com>
2014-12-01 22:52:41 -0800

    update r code

--- Stat 154 Project.R
+++ Stat 154 Project.R
@@ -1,17 +1,18 @@
 library(e1071)
 
 #Import data from csv
-
 data = read.csv('Desktop/stat154_final_proj/svm_features.csv')
 original.data = data
 head(data)
 
+
+data = original.data
 #Size of data
 nrow(data)
 ncol(data)
 
 count = rep(NA, ncol(data))
-for (i in 1:ncol(data)) {
+for (i in 1:(ncol(data)-1)) {
   count[i] = sum(data[,i]!=0)
 }
 
@@ -23,13 +24,12 @@ index = 1:nrow(data)
 test.index = sample(index, trunc(length(index)/3))
 test.set = data[test.index,]
 train.set = data[-test.index,]
-test.label = label[test.index]
-train.label = label[-test.index]
 
 
 
 
-tune.svm(isHam~., data = data, gamma = 2^(-3:3), cost = 2^(1:6))
+
+#tune.svm(isHam~., data = data, gamma = 2^(-3:3), cost = 2^(1:6))
 
 ######## RUN UP TO HERE ########
 
@@ -37,8 +37,8 @@ tune.svm(isHam~., data = data, gamma = 2^(-3:3), cost = 2^(1:6))
 svm.model <- svm(isHam ~ ., data = train.set, cost = 100, gamma = 1)
 svm.pred <- predict(svm.model, test.set)
 
-tab = table(svm.pred,label[test.index])
+tab = table(svm.pred,data$isHam[test.index])
 (tab[1,1]+tab[2,2])/sum(tab)
 tab
-sum(test.label==1)/length(test.label)
+sum(data[test.index,'isHam']=='ham')/nrow(data[test.index,])
 
