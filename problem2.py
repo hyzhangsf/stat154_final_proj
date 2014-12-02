6f752a4c699b2c1b0e1fba6f8d67117886e45d47
Hanyu Zhang <hyzhangsf@gmail.com>
2014-12-01 22:47:20 -0800

    p2.py: 2to3

--- problem2.py
+++ problem2.py
@@ -1,12 +1,10 @@
 # coding: utf-8
 __author__ = 'zz'
 import re
+from collections import Counter
 fin = open('train_msgs.txt', 'r')
 fout = open('svm_features.csv', 'w')
-stopwords = {}
-contractions = {}
-dictionary = {}
-res = []
+
 
 stopWordList = ["a", "n", "able", "about", "across", "after", "all", "almost",
              "also", "am", "among", "an", "and", "any", "are", "as", "at", "be",
@@ -19,6 +17,7 @@ stopWordList = ["a", "n", "able", "about", "across", "after", "all", "almost",
              "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these",
              "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where",
              "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
+
 
 stopWordSet = set(stopWordList)
 
@@ -33,13 +32,18 @@ def find_features(lines):
     features = set()
     featuresInlines = []
     rawLines = []
+    counter = Counter()
     for line in lines:
         line = line.lower()
         rawLines.append(line)
         line = re.sub("[^0-9a-zA-Z]+", ' ', line)
         words = [word for word in line.split()[1:] if word not in stopWordSet]
+        words = [word for word in words if not word.isdigit()]
         features.update(words)
         featuresInlines.append(words)
+        for word in words:
+            counter[word] += 1
+    print (counter.most_common(40))
     return rawLines, featuresInlines, list(features - stopWordSet)
 
 def saveMatrix(rawLines, featuresInlines, features):
@@ -53,7 +57,7 @@ def saveMatrix(rawLines, featuresInlines, features):
             row = ','.join([str(1.0*line.count(feature)/len(line)) for feature in features]) + ','+label + '\n'
             rows.append(row)
     fout.writelines(rows)
-    print '{0} features'.format(len(features))
-    print 'wrote {0} lines to {1}'.format(len(rows), 'svm_features.csv')
+    print ('{0} features'.format(len(features)))
+    print ('wrote {0} lines to {1}'.format(len(rows), 'svm_features.csv'))
 
 saveMatrix(*find_features(lines))
