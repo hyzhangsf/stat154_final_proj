659cf7b8a0e8411b8e5d7e20bd7d9d33af788c1d
kenny0805 <gdgzwkm@gmail.com>
2014-12-06 15:51:58 -0800

    power features

--- Stat 154 Project.R
+++ Stat 154 Project.R
@@ -1,16 +1,25 @@
 library(e1071)
+library(ROCR)
+
 
 #Import data from csv
-# data = read.csv('~/stat154_final_proj/svm_features.csv')
-data = read.table('~/stat154_final_proj/svm_features.csv', sep=",", header=FALSE, encoding="UTF-8",stringsAsFactors=FALSE)
-original.data = data
-head(data)
+data = read.csv('~/Desktop/stat154_final_proj/svm_features.csv')
+test = read.csv('~/Desktop/stat154_final_proj/svm_test.csv')
 
+power.data = read.csv('~/Desktop/stat154_final_proj/train_pwr.csv')
+#data = read.table('~/Desktop/stat154_final_proj/svm_features.csv', sep=",", header=TRUE, encoding="UTF-8",stringsAsFactors=FALSE)
+original.data = data
+original.test = test
+original.power.data = power.data
+head(data)
+head(test)
 
 data = original.data
+test = original.test
 #Size of data
 nrow(data)
 ncol(data)
+
 
 count = rep(NA, ncol(data))
 for (i in 1:(ncol(data)-1)) {
@@ -18,24 +27,41 @@ for (i in 1:(ncol(data)-1)) {
 }
 
 data = data[,-which(count<20)]
+test = test[,-which(count<20)]
 nrow(data)
 ncol(data)
 
-index = 1:nrow(data)
-test.index = sample(index, trunc(length(index)/3))
-test.set = data[test.index,]
-train.set = data[-test.index,]
-
-#tune.svm(isHam~., data = data, gamma = 2^(-3:3), cost = 2^(1:6))
 
 ######## RUN UP TO HERE ########
 
 
-svm.model <- svm(isHam ~ ., data = train.set, cost = 100, gamma = 1)
-svm.pred <- predict(svm.model, test.set)
+svm.model <- svm(isHam ~ ., data = data, cost = 100, gamma = 0.0001)
+svm.pred <- predict(svm.model, test)
 
-tab = table(svm.pred,data$isHam[test.index])
-(tab[1,1]+tab[2,2])/sum(tab)
+
+tab = table(svm.pred,test$isHam)
 tab
-sum(data[test.index,'isHam']=='ham')/nrow(data[test.index,])
+(tab[1,1]+tab[2,2])/sum(tab)
+sum(test[,'isHam']=='ham')/nrow(test)
 
+
+
+head(attributes(svm.pred)$decision.values))
+head(svm.model$decision.values)
+svm.model$decision.values
+
+
+
+######## POWER FEATURES #########
+
+test.index = sample(1:floor(nrow(power.data)/3))
+train.index = -test.index
+svm.model <- svm(class_label ~ ., data = power.data[train.index,], cost = 100, gamma = 0.0001)
+svm.pred <- predict(svm.model, power.data[test.index,])
+
+tab = table(svm.pred,power.data[test.index, 'class_label'])
+tab
+(tab[1,1]+tab[2,2])/sum(tab)
+sum(test[,'isHam']=='ham')/nrow(test)
+
+test.index[which(svm.pred != power.data[test.index, 'class_label'])]
--- parseMsgs.py
+++ parseMsgs.py
@@ -1,70 +1,101 @@
 #coding: utf-8
 import re
+from powerFeature import ALL_FUNCTIONS
+#input file
 fin=open('train_msgs.txt','r')
-fout=open('output.csv','w')
+#output file
+fout1=open('train_word.csv','w')
+fout2=open('train_pwr.csv','w')
 stopwords={}
 contractions={}
 dictionary={}
 res=[]
 
-for word in ["a","n","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"]:
-	stopwords[word]=None
-for contraction in ["dont","won't","can't","isn't","aren't","wasn't","weren't","haven't","hasn't","hadn't","won't","wouldn't","don't","doesn't","didn't","can't","couldn't","shouldn't","mightn't","mustn't"]:
-	contractions[contraction]=None
+fout2.write("class_label,"+",".join(['f'+str(i+1) for i in range(len(ALL_FUNCTIONS))]))
+
+STOPWORDS=set(["a","n","able","about","across","after","all","almost","also",
+	"am","among","an","and","any","are","as","at","be","because","been","but","by",
+	"can","cannot","could","dear","did","do","does","either","else","ever","every",
+	"for","from","get","got","had","has","have","he","her","hers","him","his","how",
+	"however","i","if","in","into","is","it","its","just","least","let","like",
+	"likely","may","me","might","most","must","my","neither","no","nor","not","of",
+	"off","often","on","only","or","other","our","own","rather","said","say","says",
+	"she","should","since","so","some","than","that","the","their","them","then",
+	"there","these","they","this","tis","to","too","twas","us","wants","was","we",
+	"were","what","when","where","which","while","who","whom","why","will","with",
+	"would","yet","you","your"])
+
+#removes contraction in a line of input
+def restore_contraciton(line):
+	line=re.sub("'s","",line)
+	line=re.sub("won't|wont","will not",line)
+	line=re.sub("dont","do not",line)
+	line=re.sub("can't","cannot",line)
+	line=re.sub("n't"," not",line)
+	line=re.sub("'m"," am",line)
+	line=re.sub("'ve"," have",line)
+	line=re.sub("'re"," are",line)
+	line=re.sub("'d"," would",line)
+	line=re.sub("'ll"," will",line)
+	return line
+
 for line in fin:
+	#extract and write all power features:
+	tmp=line.split()
+	class_label=tmp[0]
+	line=" ".join(tmp[1:])
+
+	pwr = [str(f(line)) for f in ALL_FUNCTIONS]
+	fout2.write("\n"+class_label+","+",".join(pwr))
+
 	#all lowercase
 	line=line.lower()
 	
 	#remove contractions
-	line=re.sub("'m|'s|'ve|'re|'d|'ll","",line)
-	tmp=line.split()
-	for i in range(len(tmp)):
-		if tmp[i] in contractions:
-			tmp[i]=""
-	line=" ".join(tmp)
+	line=restore_contraciton(line)
 	
-	#replace all nonalphanumeric
-	line=re.sub("[^0-9a-z]"," ",line)
-	
-	#remove numeric sequences
-	line=re.sub("[0-9]+ [0-9]+"," ",line)
-	line=re.sub("[0-9]{5,50}"," ",line)
-
 	#remove alphanumeric words: eg.kagbew2433
 	line=re.sub("([a-z]+[0-9]+|[0-9]+[a-z]+)[^\s\-,]*"," ",line)
 
+	#replace all nonwords
+	line=re.sub("[^a-z]"," ",line)
+		
 	#remove extra space
 	line=re.sub(" +"," ",line)
 	
 	#remove all stopwords
 	#process all remaining words
-	print line
+	#dictionary is stored as word:index pairs
+	#each msg is parsed as its classification followed by a list of indices
 	tmp=line.split()
-	msg=["s" if tmp[0]=="spam" else "h"]
-	for i in range(1,len(tmp)):
-		if tmp[i] not in stopwords:
-			if tmp[i] not in dictionary:
-				dictionary[tmp[i]]=len(dictionary)
-			msg.append(dictionary[tmp[i]])
+	msg=[class_label]
+	pwr=[]
+	for i in range(len(tmp)):
+		if tmp[i] not in dictionary:
+			dictionary[tmp[i]]=len(dictionary)
+		msg.append(dictionary[tmp[i]])
+	res.append(msg)
 
-	if(len(msg)>1):
-		res.append(msg)
-
-
+#first line of input, lists all word features
 allwords=[None]*len(dictionary)
 for word in dictionary:
 	allwords[dictionary[word]]=word
-allwords=re.sub("[ \']","",str(allwords))
-fout.write("class,"+allwords[1:(len(allwords)-1)])
+allwords=",".join(allwords)
+fout1.write("class,"+allwords)
 
+#writes the frequency of each message
 for msg in res:
 	tmp=[0]*len(dictionary)
-	for i in range(1,len(msg)):
-		tmp[msg[i]]+=1.0/(len(msg)-1)
-	tmp=str(tmp)
-	tmp=re.sub(" ","",tmp)
-	fout.write("\n"+msg[0]+","+tmp[1:(len(tmp)-1)])
-#prints out the number of columns
+	if len(msg)>1:
+		for i in range(1,len(msg)):
+			tmp[msg[i]]+=1.0/(len(msg)-1)
+	tmp=",".join(map(str, tmp))
+	fout1.write("\n"+msg[0]+","+tmp)
+
+#prints out the number of word features
 print len(dictionary)
+
+#close all files
 fin.close()
-fout.close()
+fout1.close()
+fout2.close()
